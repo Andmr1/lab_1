d193158b4e6b3ae17c0cd5de99904871fbee1e20
Андрей <marinin1337@gmail.com>
2022-09-26 10:07:21 +0400

    Изменена задержка между итерациями

--- task.py
+++ task.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 from time import sleep
-import random
 import codecs
 count_good = 0
 count_bad = 0
@@ -11,26 +10,35 @@ def find_good(url_, pages, count_good_):
     for page in range(1, pages+1):
         print(page)
         url1 = url_ + f"/{page}/"
-        value = random.random()
-        scaled_value = 1 + (value * (9 - 5))
-        sleep(scaled_value)
+        delay_value = 30 + 2 * page
+        sleep(delay_value)
         r = requests.get(url1)
         soup = BeautifulSoup(r.text, "lxml")
         film_name = soup.find("a", class_="breadcrumbs__link")
-        revs = soup.findAll("span", class_="_reachbanner_")
-        for rev in revs:
+        sources = soup.findAll("div", class_="response good")
+        for src in sources:
+            rev = src.find("span", class_="_reachbanner_").text
             if count_good_ < 10:
                 name = "000" + str(count_good_)
-            elif 10 < count_good_ < 100:
-                name = "00" + str(count_good_)
-            elif count_good_ > 100:
-                name = "0" + str(count_good_)
-            if count_good_ < 999:
-                file = codecs.open(u''+"dataset/good/"+name+".txt", "w", "utf-8")
+                file = codecs.open(u'' + "dataset/good/" + name + ".txt", "w", "utf-8")
                 file.write(film_name.text + "\n")
-                file.write(rev.text)
-                file.close
-            count_good_ += 1
+                file.write(rev)
+                file.close()
+                count_good_ += 1
+            elif 10 <= count_good_ < 100:
+                name = "00" + str(count_good_)
+                file = codecs.open(u'' + "dataset/good/" + name + ".txt", "w", "utf-8")
+                file.write(film_name.text + "\n")
+                file.write(rev)
+                file.close()
+                count_good_ += 1
+            elif 100 <= count_good_ <= 999:
+                name = "0" + str(count_good_)
+                file = codecs.open(u'' + "dataset/good/" + name + ".txt", "w", "utf-8")
+                file.write(film_name.text + "\n")
+                file.write(rev)
+                file.close()
+                count_good_ += 1
     return count_good_
 
 
@@ -38,26 +46,35 @@ def find_bad(url_, pages, count_bad_):
     for page in range(1, pages+1):
         print(page)
         url1 = url_ + f"/{page}/"
-        value = random.random()
-        scaled_value = 1 + (value * (9 - 5))
-        sleep(scaled_value)
+        delay_value = 30 + 2 * page
+        sleep(delay_value)
         r = requests.get(url1)
         soup = BeautifulSoup(r.text, "lxml")
         film_name = soup.find("a", class_="breadcrumbs__link")
-        revs = soup.findAll("span", class_="_reachbanner_")
-        for rev in revs:
+        sources = soup.findAll("div", class_="response bad")
+        for src in sources:
+            rev = src.find("span", class_="_reachbanner_").text
             if count_bad_ < 10:
                 name = "000" + str(count_bad_)
-            elif 10 < count_bad_ < 100:
-                name = "00" + str(count_bad_)
-            elif count_bad_ > 100:
-                name = "0" + str(count_bad_)
-            if count_bad_ < 999:
-                file = codecs.open(u''+"dataset/bad/"+name+".txt", "w", "utf-8")
+                file = codecs.open(u'' + "dataset/bad/" + name + ".txt", "w", "utf-8")
                 file.write(film_name.text + "\n")
-                file.write(rev.text)
-                file.close
-            count_bad_ += 1
+                file.write(rev)
+                file.close()
+                count_bad_ += 1
+            elif 10 <= count_bad_ < 100:
+                name = "00" + str(count_bad_)
+                file = codecs.open(u'' + "dataset/bad/" + name + ".txt", "w", "utf-8")
+                file.write(film_name.text + "\n")
+                file.write(rev)
+                file.close()
+                count_bad_ += 1
+            elif 100 <= count_bad_ <= 999:
+                name = "0" + str(count_bad_)
+                file = codecs.open(u'' + "dataset/bad/" + name + ".txt", "w", "utf-8")
+                file.write(film_name.text + "\n")
+                file.write(rev)
+                file.close()
+                count_bad_ += 1
     return count_bad_
 
 
@@ -65,41 +82,50 @@ def find_bad_new(url_, pages, count_bad_):
     for page in range(1, pages+1):
         print(page)
         url1 = url_ + f"/{page}/"
-        value = random.random()
-        scaled_value = 1 + (value * (9 - 5))
-        sleep(scaled_value)
+        delay_value = 30 + 2 * page
+        sleep(delay_value)
         r = requests.get(url1)
         soup = BeautifulSoup(r.text, "lxml")
         sources = soup.findAll("div", class_="response bad")
         for source in sources:
+            film_name = source.find("p", class_="film").text
+            rev = source.find("span", class_="_reachbanner_").text
             if count_bad_ < 10:
                 name = "000" + str(count_bad_)
-            elif 10 < count_bad_ < 100:
-                name = "00" + str(count_bad_)
-            elif count_bad_ > 100:
-                name = "0" + str(count_bad_)
-            film_name = source.find("p", class_="film").get("span")
-            rev = source.find("span", class_="_reachbanner_").text
-            if count_bad_ < 999:
-                file = codecs.open(u''+"dataset/bad/"+name+".txt", "w", "utf-8")
-                file.write(str(film_name) + "\n")
+                file = codecs.open(u'' + "dataset/bad/" + name + ".txt", "w", "utf-8")
+                file.write(film_name + "\n")
                 file.write(rev)
-                file.close
-            count_bad_ += 1
+                file.close()
+                count_bad_ += 1
+            elif 10 <= count_bad_ < 100:
+                name = "00" + str(count_bad_)
+                file = codecs.open(u'' + "dataset/bad/" + name + ".txt", "w", "utf-8")
+                file.write(film_name + "\n")
+                file.write(rev)
+                file.close()
+                count_bad_ += 1
+            elif 100 <= count_bad_ <= 999:
+                name = "0" + str(count_bad_)
+                file = codecs.open(u'' + "dataset/bad/" + name + ".txt", "w", "utf-8")
+                file.write(film_name + "\n")
+                file.write(rev)
+                file.close()
+                count_bad_ += 1
     return count_bad_
 
 
-count_good = find_good("https://www.kinopoisk.ru/film/448/reviews/ord/rating/status/good/perpage/200", 4, count_good)
-count_good = find_good("https://www.kinopoisk.ru/film/258687/reviews/ord/rating/status/good/perpage/200", 4,
+count_good = find_good("https://www.kinopoisk.ru/film/448/reviews/ord/rating/status/good/perpage/25", 32, count_good)
+count_good = find_good("https://www.kinopoisk.ru/film/258687/reviews/ord/rating/status/good/perpage/25", 32,
                        count_good)
-count_bad = find_bad("https://www.kinopoisk.ru/film/623250/reviews/ord/date/status/bad/perpage/75", 1, count_bad)
-count_bad = find_bad("https://www.kinopoisk.ru/film/278522/reviews/?status=bad&ord=date&rnd=1664127127&perpage=100", 1,
+count_bad = find_bad("https://www.kinopoisk.ru/film/623250/reviews/ord/date/status/bad/perpage/25", 4, count_bad)
+count_bad = find_bad("https://www.kinopoisk.ru/film/278522/reviews/?status=bad&ord=date&rnd=1664127127&perpage=25", 4,
                      count_bad)
-count_bad = find_bad("https://www.kinopoisk.ru/film/819101/reviews/ord/date/status/bad/perpage/50", 1, count_bad)
-count_bad = find_bad("https://www.kinopoisk.ru/film/718222/reviews/ord/date/status/bad/perpage/100", 1, count_bad)
-count_bad = find_bad("https://www.kinopoisk.ru/film/718223/reviews/ord/date/status/bad/perpage/200", 1, count_bad)
-count_bad = find_bad("https://www.kinopoisk.ru/film/407636/reviews/ord/rating/status/bad/perpage/75", 1, count_bad)
-count_bad = find_bad("https://www.kinopoisk.ru/film/277328/reviews/ord/rating/status/bad/perpage/75", 1, count_bad)
-count_bad = find_bad("https://www.kinopoisk.ru/film/258687/reviews/?status=bad&ord=rating&rnd=1664126777&perpage=200",
-                     1, count_bad)
-count_bad = find_bad_new("https://www.kinopoisk.ru/reviews/type/comment/status/bad/period/month/page", 2, count_bad)
+count_bad = find_bad("https://www.kinopoisk.ru/film/819101/reviews/ord/date/status/bad/perpage/25", 2, count_bad)
+count_bad = find_bad("https://www.kinopoisk.ru/film/718222/reviews/ord/date/status/bad/perpage/25", 2, count_bad)
+count_bad = find_bad("https://www.kinopoisk.ru/film/718223/reviews/ord/date/status/bad/perpage/25", 8, count_bad)
+count_bad = find_bad("https://www.kinopoisk.ru/film/407636/reviews/ord/rating/status/bad/perpage/25", 3, count_bad)
+count_bad = find_bad("https://www.kinopoisk.ru/film/277328/reviews/ord/rating/status/bad/perpage/25", 3, count_bad)
+count_bad = find_bad("https://www.kinopoisk.ru/film/258687/reviews/?status=bad&ord=rating&rnd=1664126777&perpage=25",
+                     4, count_bad)
+count_bad = find_bad_new("https://www.kinopoisk.ru/reviews/type/comment/status/bad/period/month/perpage/25", 16,
+                         count_bad)
